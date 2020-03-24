68bb9a0ea350f6ddccb0b73d5395f83ccb0d242e
quentin.mino <qmino@octo.com>
2020-03-24 17:52:04 +0100

    logging request and response with headers

--- src/main/java/com/example/poclog/PoclogApplication.java
+++ src/main/java/com/example/poclog/PoclogApplication.java
@@ -2,12 +2,25 @@ package com.example.poclog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @SpringBootApplication
 public class PoclogApplication {
 
+    @Bean
+    public CommonsRequestLoggingFilter logFilter() {
+        CommonsRequestLoggingFilter filter
+                = new CommonsRequestLoggingFilter();
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(true);
+        filter.setMaxPayloadLength(10000);
+        filter.setIncludeHeaders(true);
+        filter.setAfterMessagePrefix("REQUEST DATA : ");
+        return filter;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(PoclogApplication.class, args);
     }
-
 }
